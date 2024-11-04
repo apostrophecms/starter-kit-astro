f9dd5c6ef9a350171458d1f04cfd5f6b09965b3b
Harouna Traoré <haroun@users.noreply.github.com>
2024-11-04 19:30:24 +0100

    revert no so secret secret

--- modules/@apostrophecms/express/index.js
+++ modules/@apostrophecms/express/index.js
@@ -2,7 +2,7 @@ export default {
   options: {
     session: {
       // If this still says `undefined`, set a real secret!
-      secret: 'This is a real secret!'
+      secret: undefined
     }
   }
 };
