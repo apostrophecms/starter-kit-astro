80984020e839f9318b5712139c64d5d33056805a
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2024-11-08 14:46:01 +0200

    Disable HMR and polyfill

--- modules/@apostrophecms/asset/index.js
+++ modules/@apostrophecms/asset/index.js
@@ -2,6 +2,10 @@ export default {
   // When not in production, refresh the page on restart
   options: {
     refreshOnRestart: true,
-    hmr: 'apos'
+    // HMR not working with Astro yet, because
+    // Vite can't handle 2 HMR clients
+    hmr: false,
+    // Disable the module preload polyfill
+    modulePreloadPolyfill: false
   }
 };
